0d602f7201caebab190443d4d5ed07fd6fb6e3ba
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-27 01:28:29 +0500

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -101,10 +101,10 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2">
         <motion.div
           animate={{
-            y: [0, 24, 0],
+            y: [0, 18, 0],
           }}
           transition={{
             duration: 1.5,
