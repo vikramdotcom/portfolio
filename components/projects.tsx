c74fc7eddce38c879abc9933ff8756056c9c00d1
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-05-02 23:29:52 +0500

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -175,7 +175,7 @@ const projects: Project[] = [
     technologies: ["React", "TypeScript", "nextjs", "CSS Modules", "tailwind"],
     features: [
       "User authentication",
-      "Book reviews and ratings",sasasasasasssaas
+      "Book reviews and ratings",
       "Wishlist and favorites",
       "Order history and tracking",
       "Responsive design for all devices",
