9293ceba936ff001d6c94aa3c0009fbdeeae8184
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-05-04 23:32:24 +0500

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -25,7 +25,7 @@ interface Project {
 
 const projects: Project[] = [
 
-  {bvnkhgkkhnmkjkgjf
+  {
     id: "1",
     title: "Avion Ecommerce Marketplace",
     subtitle: "Your One-Stop Shop for Everything!",
