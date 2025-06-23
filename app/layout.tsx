173d5aee706d14e06bb95633fddb0b14993bcb86
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-06-23 17:31:54 +0500

    Add Vercel Analytics to layout
    Integrated @vercel/analytics into the project by installing the package and adding the <Analytics /> component to the root layout. This enables analytics tracking across the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Poppins } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ['latin'], 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <Navbar/>
         {children}
+         <Analytics />
         <Footer/>
       </body>
     </html>
