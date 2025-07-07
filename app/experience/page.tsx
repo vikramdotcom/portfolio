22d3ac9409d742c58221779949220fa92f3419ee
vikram-singh9 <163000375+vikramdotcom@users.noreply.github.com>
2025-07-07 19:55:53 +0500

    Add experience page and update layout and hero
    Created a new experience page with WorkExperience component. Updated layout to change the site title, adjust Navbar spacing, and remove unused local font import. Minor style fix in HeroSection. Removed unused image dsdsd.png.

---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WorkExperience from '@/components/experience'
+const experiences = () => {
+  return (
+    <div>
+        <WorkExperience />
+    </div>
+  )
+}
+
+export default experiences
